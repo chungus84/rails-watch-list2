25118f958a943b64b5861b182c76fd442b0f1947
Kenichi Beveridge <kenbev84@gmail.com>
2022-07-26 20:45:04 +0100

    Contructed my models and migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts 'Purging Movie Database'
+Movie.destroy_all
+puts 'Purge complete'
+
+puts 'reading movie API'
+response = JSON.parse(URI.open('https://tmdb.lewagon.com/movie/top_rated').read)
+
+puts 'looping through API response'
+response['results'].each do |movie|
+  puts "creating #{movie['title']}"
+  Movie.create!(title: movie['title'],
+                overview: movie['overview'],
+                poster_url: "https://image.tmdb.org/t/p/w500/#{movie['backdrop_path']}",
+                rating: movie['vote_average'])
+end
+puts 'seeding complete'
