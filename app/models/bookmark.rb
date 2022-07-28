f0a8ee946d0c58b7be29b71af5a03d45c3847c6e
Kenichi Beveridge <kenbev84@gmail.com>
2022-07-28 19:35:33 +0100

    Added bookmark Controller

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,6 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-  validates_presence_of :movie, :list
-  validates :comment, length: { minimum: 6 }
-  validates :movie, uniqueness: { scope: :list, message: "This Movie is already on the list"}
+  validates_length_of :comment, { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id, message: "This Movie is already on the list"}
 end
